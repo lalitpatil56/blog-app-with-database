03e7ed7b581315a54511589d9cc857374533ee7b
Lalit <lalit.patil5694@gmail.com>
2021-05-01 15:59:05 +0530

    added port procfile dependencies

--- app.js
+++ app.js
@@ -95,6 +95,6 @@ app.get("/posts/:postId", function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function () {
+  console.log("Server started on Heroku server.");
 });
